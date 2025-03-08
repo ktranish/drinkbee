0604fa48f727fb50502177dec72acddb51ead71f
Kenny Tran <ktranish@outlook.com>
2025-03-08 19:45:06 +0100

    wip

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager - Loaded after interactive */}
+        <Script
+          id="google-tag-manager"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NFBSXMSF');
+            `,
+          }}
+        />
+      </head>
       <body className={cn(inter.className, "flex min-h-screen flex-col")}>
         <ThemeProvider
           attribute="class"
@@ -36,4 +52,5 @@ export default function RootLayout({
   );
 }
 
+import Script from "next/script";
 import "./globals.css";
